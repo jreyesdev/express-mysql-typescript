462f852ecde7d751e77fb9294b4a7eb514371dd5
jreyesdev <djrm1204@gmail.com>
2021-10-23 00:06:32 -0400

    Config conexion a mysql

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import Server from './server/server'
+import MySql from './mysql/mysql'
 
 const server = Server.init()
+const db = new MySql()
 
 server.start()
---
+++ src/mysql/mysql.ts
@@ -0,0 +1,27 @@
+import mysql from 'mysql'
+
+export default class MySql{
+    private static _instance: MySql
+    cnn: mysql.Connection
+    conectado: boolean = false
+
+    constructor(){
+        console.log('Mysql inicializada')
+        this.cnn = mysql.createConnection({
+            host: process.env.DB_HOST,
+            port: Number(process.env.DB_PORT),
+            database: process.env.DB_NAME,
+            user: process.env.DB_USERNAME,
+            password: process.env.DB_PASS
+        })
+        this.conectarDB()
+    }
+
+    private conectarDB(){
+        this.cnn.connect((err: mysql.MysqlError) => {
+            if(err) throw new Error(err.message)
+            this.conectado = true
+            console.log('BD is connected')
+        })
+    }
+}
